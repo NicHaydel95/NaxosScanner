82b5fd8dd11588f244633e12af6d38f8d0a33e08
NicHaydel95 <nhaydel@nd.edu>
2017-04-10 09:18:55 -0400

    Updated back button behavior in WebViewActivity

--- app/src/main/java/com/example/nhaydel/naxosscanner/WebViewActivity.java
+++ app/src/main/java/com/example/nhaydel/naxosscanner/WebViewActivity.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
-
+import android.webkit.WebViewClient;
 
 
 public class WebViewActivity extends AppCompatActivity {
@@ -20,8 +20,9 @@ public class WebViewActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         url = getIntent().getStringExtra("URL");
-        webView = new WebView(getBaseContext());
+        webView = new WebView(getApplicationContext());
         webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
         webView.setVisibility(View.VISIBLE);
         webView.loadUrl(url);
