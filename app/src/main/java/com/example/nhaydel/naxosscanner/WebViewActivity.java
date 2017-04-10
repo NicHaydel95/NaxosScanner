f62557b9f82282c3f34ac1dd3c1ce281e9712ce8
NicHaydel95 <nhaydel@nd.edu>
2017-04-10 11:35:26 -0400

    Updated web view and back button behavior

--- app/src/main/java/com/example/nhaydel/naxosscanner/WebViewActivity.java
+++ app/src/main/java/com/example/nhaydel/naxosscanner/WebViewActivity.java
@@ -1,13 +1,11 @@
 package com.example.nhaydel.naxosscanner;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
+
 
 
 public class WebViewActivity extends AppCompatActivity {
@@ -22,17 +20,15 @@ public class WebViewActivity extends AppCompatActivity {
         url = getIntent().getStringExtra("URL");
         webView = new WebView(getApplicationContext());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
         setContentView(webView);
         webView.setVisibility(View.VISIBLE);
         webView.loadUrl(url);
     }
+
     @Override
-    public void onBackPressed() {
-        if (webView.canGoBack()) {
-            webView.goBack();
-        } else {
-            super.onBackPressed();
-        }
+    public void onResume(){
+        super.onResume();
+        this.finish();
     }
+
 }
